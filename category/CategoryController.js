17bb15a9e15e24da3dd0173a7bd6963f4185fd42
Daniel Moraes <moraesdeveloper@gmail.com>
2020-05-28 12:36:39 -0300

    Insertion of checkout and category

--- category/CategoryController.js
+++ category/CategoryController.js
@@ -1,8 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const authentication = require("../middleware/authentication");
+const category = require("./Category");
 
 router.get("/categories", (req, res) => {
     res.send("Rota de categorias");
+});
+
+router.post("/category", authentication, (req, res) => {
+    var description = req.body.description.toUpperCase();
+    var userId = req.userId;
+
+    if ((description == undefined) || (description == "")) {
+        res.statusCode = 400;
+        res.json({ error: "Invalid description" });
+        return;
+    }
+
+    category.findOne({
+        where: {
+            description: description,
+            userId: userId
+        }
+    }).then(categoryFound => {
+        if (categoryFound != undefined) {
+            res.statusCode = 400;
+            res.json({ error: "Category already exists!" });
+        } else {
+            category.create({
+                description: description,
+                userId: userId
+            }).then(() => {
+                res.statusCode = 200;
+                res.json({ msg: "Success!" });
+            });
+        }
+    });
 });
 
 module.exports = router;
--- checkout/CheckoutController.js
+++ checkout/CheckoutController.js
@@ -1,8 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const checkout = require("./Checkout");
+const authentication = require("../middleware/authentication");
 
 router.get("/checkouts", (req, res) => {
     res.send("Rota de checkouts");
+});
+
+
+router.post("/checkout", authentication, (req, res) => {
+    var description = req.body.description.toUpperCase();
+    var userId = req.userId;
+
+    if ((description == undefined) || (description == "")) {
+        res.statusCode = 400;
+        res.json({ error: "Invalid description" });
+        return;
+    }
+
+    checkout.findOne({
+        where: {
+            description: description,
+            userId: userId
+        }
+    }).then(checkoutFound => {
+        if (checkoutFound != undefined) {
+            res.statusCode = 400;
+            res.json({ error: "Checkout already exists!" });
+        } else {
+            checkout.create({
+                description: description,
+                userId: userId
+            }).then(() => {
+                res.statusCode = 200;
+                res.json({ msg: "Success!" });
+            });
+        }
+    });
 });
 
 module.exports = router;
