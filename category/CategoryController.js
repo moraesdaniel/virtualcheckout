1d6d456df077ca3532cdb206d6a864701e3b6e16
Daniel Moraes <moraesdeveloper@gmail.com>
2020-05-29 20:24:58 -0300

    Improve categoryController class

--- category/CategoryController.js
+++ category/CategoryController.js
@@ -2,8 +2,85 @@ const express = require("express");
 const router = express.Router();
 const authentication = require("../middleware/authentication");
 const category = require("./Category");
+const sequelize = require("sequelize");
+const { or, and, gt, lt, ne } = sequelize.Op;
 
-router.get("/categories", authentication, (req, res) => {
+function ValidateCategory(description, userId, categoryId) {
+    return new Promise((resolve, reject) => {
+        if ((description == undefined) || (description.trim() == "")) {
+            reject("Invalid description!" );
+        }
+
+        if (isNaN(categoryId)) {
+            reject("Invalid id!");
+        }
+
+        category.findOne({
+            where: {
+                description: description.toUpperCase(),
+                userId: userId,
+                id: {[ne]: categoryId}
+            }
+        }).then(categoryFound => {
+            if (categoryFound != undefined) {
+                reject("Category already exists!");
+            } else {
+                resolve("Success!");
+            }
+        }).catch((msgErro) => {
+            reject(msgErro);
+        });
+    });
+}
+
+function AddCategory(req, res) {
+    var description = req.body.description;
+    var userId = req.userId;
+
+    ValidateCategory(description, userId, 0).then(() => {
+        category.create({
+            description: description,
+            userId: userId
+        }).then(() => {
+            res.statusCode = 200;
+            res.json({ msg: "Success!" });
+        }).catch((error) => {
+            res.statusCode = 500;
+            res.json({ error: error });
+        });
+    }).catch((msgError) => {
+        res.statusCode = 400;
+        res.json({ error: msgError });
+    });
+}
+
+function UpdateCategory(req, res) {
+    var userId = req.userId;
+    var { description, id } = req.body;
+
+    ValidateCategory(description, userId, id).then(() => {
+        category.update(
+            { description: description.toUpperCase() },
+            { where: { id: id } }
+        ).then((rowsUpdated) => {
+            if (rowsUpdated != 0) {
+                res.statusCode = 200;
+                res.json({ msg: "Success!" });
+            } else {
+                res.statusCode = 400;
+                res.json({ msg: "Category not found!" });
+            }
+        }).catch((error) => {
+            res.statusCode = 500;
+            res.json({ error: error });
+        });
+    }).catch((msgError) => {
+        res.statusCode = 400;
+        res.json({ error: msgError });
+    });
+}
+
+function GetCategories(req, res) {
     var userId = req.userId;
     category.findAll({
         where: {
@@ -17,38 +94,29 @@ router.get("/categories", authentication, (req, res) => {
     }).catch((msgError) => {
         res.statusCode = 500;
         res.json({ error: msgError });
-    });
+    }); 
+}
+
+function DeleteCategory(req, res) {
+    //Implementar amanhã ThereIsMovementsCategory
+}
+
+//Routes
+router.get("/categories", authentication, (req, res) => {
+    GetCategories(req, res);
 });
 
+
 router.post("/category", authentication, (req, res) => {
-    var description = req.body.description.toUpperCase();
-    var userId = req.userId;
+    AddCategory(req, res);
+});
 
-    if ((description == undefined) || (description == "")) {
-        res.statusCode = 400;
-        res.json({ error: "Invalid description" });
-        return;
-    }
+router.put("/category", authentication, (req, res) => {
+    UpdateCategory(req, res);
+});
 
-    category.findOne({
-        where: {
-            description: description,
-            userId: userId
-        }
-    }).then(categoryFound => {
-        if (categoryFound != undefined) {
-            res.statusCode = 400;
-            res.json({ error: "Category already exists!" });
-        } else {
-            category.create({
-                description: description,
-                userId: userId
-            }).then(() => {
-                res.statusCode = 200;
-                res.json({ msg: "Success!" });
-            });
-        }
-    });
+router.delete("/category", authentication, (req, res) => {
+    DeleteCategory(req, res);
 });
 
 module.exports = router;
