7adde910b0c4c3a12c4fe98e078243f3dbc8dc03
Daniel Moraes <moraesdeveloper@gmail.com>
2020-06-02 10:21:56 -0300

    Improving the class of the connection

--- dao/connection.js
+++ dao/connection.js
@@ -1,7 +1,11 @@
 const sequelize = require("sequelize");
+const DBName = "caixavirtual";
+const user = "root";
+const password = "m@ster2020";
+const host = "localhost";
 
-const connection = new sequelize("caixavirtual", "root", "m@ster2020", {
-    host: "localhost",
+const connection = new sequelize(DBName, user, password, {
+    host: host,
     dialect: "mysql"
 });
 
